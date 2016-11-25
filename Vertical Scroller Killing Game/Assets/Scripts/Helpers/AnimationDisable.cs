0c41a386255f25932a78eba4ec8fbfd528f212ca
Jorge M. Carvalho <JorgeMiguelSCarvalho@gmail.com>
2016-11-25 22:37:40 +0000

    Added base files. Starting commit. Get to work chumps.

---
+++ Vertical Scroller Killing Game/Assets/Scripts/Helpers/AnimationDisable.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Used to disable objects at the end of animations.
+public class AnimationDisable : MonoBehaviour {
+
+    // Disables the animation.
+	public void DisableObject()
+    {
+        gameObject.SetActive(false);
+    }
+}
