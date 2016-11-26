9f6be3dc87b9757e844d42d6744babd808a69338
Jorge M. Carvalho <JorgeMiguelSCarvalho@gmail.com>
2016-11-26 02:44:37 +0000

    Added Comments.

--- Vertical Scroller Killing Game/Assets/Scripts/Platforms.cs
+++ Vertical Scroller Killing Game/Assets/Scripts/Platforms.cs
@@ -1,27 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+// Creates platforms for generation.
 public class Platforms : MonoBehaviour {
 
+    [Header("Spawning Properties")]
 	public float Speed = 0.1f;
 	public GameObject prefab;
-	public GameObject paizinho;
-	bool cooldown = false;
+	public Transform paizinho;
 
+    // Internal.
+    private bool _coolDown = false;
+
+    // Update is called every frame
 	void Update () {
-
+        // Translates this platform.
 	    transform.Translate(Vector2.down * Time.deltaTime * Speed);
-		if (!cooldown) {
-			StartCoroutine (SPAWN ());
+		if (!_coolDown) {
+			StartCoroutine (CreateNext ());
 		}
 	}
 
-	IEnumerator SPAWN()
+    // Creates the next set of platforms.
+	IEnumerator CreateNext()
 	{   
-		cooldown = true;
+		_coolDown = true;
 		yield return new WaitForSeconds (5f);
-		GameObject instance = (GameObject) Instantiate (prefab,transform.SetParent(paizinho));
-		//prefab.transform.SetParent (paizinho);
-		cooldown = false;
+		GameObject instance = (GameObject) Instantiate (prefab, transform.position, Quaternion.identity));
+		prefab.transform.SetParent (paizinho);
+		_coolDown = false;
 	}
 }
