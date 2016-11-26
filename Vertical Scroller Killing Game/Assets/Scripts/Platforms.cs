0a3a13da59cf07bce8939fc2bc92c334ef10745c
David Filipe <davidvfilipe@gmail.com>
2016-11-26 02:39:30 +0000

    Added Platforms Something Something Something.

---
+++ Vertical Scroller Killing Game/Assets/Scripts/Platforms.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Platforms : MonoBehaviour {
+
+	public float Speed = 0.1f;
+	public GameObject prefab;
+	public GameObject paizinho;
+	bool cooldown = false;
+
+	void Update () {
+
+	    transform.Translate(Vector2.down * Time.deltaTime * Speed);
+		if (!cooldown) {
+			StartCoroutine (SPAWN ());
+		}
+	}
+
+	IEnumerator SPAWN()
+	{   
+		cooldown = true;
+		yield return new WaitForSeconds (5f);
+		GameObject instance = (GameObject) Instantiate (prefab,transform.SetParent(paizinho));
+		//prefab.transform.SetParent (paizinho);
+		cooldown = false;
+	}
+}
