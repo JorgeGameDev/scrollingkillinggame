83f934f5a6e28f0771d1ad8d860bb91ff2ac0668
Jorge M. Carvalho <JorgeMiguelSCarvalho@gmail.com>
2016-11-27 11:21:11 +0000

    Added Icons for Every Size.

--- Vertical Scroller Killing Game/Assets/Scripts/Menu.cs
+++ Vertical Scroller Killing Game/Assets/Scripts/Menu.cs
@@ -2,18 +2,20 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-
+// Used for menu functionality.
 public class Menu : MonoBehaviour {
 
-
+    // Update is called every frame
 	void Update () 
 	{
-		if ( Input.GetButtonDown ("Joystick 1 Jump") ) 
+        // Checks for input loading the actual game scene.
+		if (Input.GetButtonDown ("Joystick 1 Jump") || Input.GetButtonDown("Joystick 2 Jump") || Input.GetButtonDown("Joystick 3 Jump") || Input.GetButtonDown("Joystick 4 Jump")) 
 		{
-				SceneManager.LoadScene ("Game 2");
+			SceneManager.LoadScene (1);
 		}
 
-		if ( Input.GetButtonDown ("Joystick 1 Attack") ) 
+        // Checks for input on quit.
+		if (Input.GetButtonDown ("Joystick 1 Attack") || Input.GetButtonDown("Joystick 2 Attack") || Input.GetButtonDown("Joystick 3 Attack") || Input.GetButtonDown("Joystick 4 Attack")) 
 		{
 			Application.Quit();
 		}
---
+++ Vertical Scroller Killing Game/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Used for playing sounds.
+public class SoundManager : MonoBehaviour {
+
+    public static SoundManager soundManager;
+    private AudioSource _audioSource;
+
+	// Use this for initialization
+	void Start ()
+    {
+	    // Assures this is the only sound manager in existence.
+        if(soundManager == null)
+        {
+            soundManager = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    // Update is called every frame.
+    void Update()
+    {
+
+    }
+}
