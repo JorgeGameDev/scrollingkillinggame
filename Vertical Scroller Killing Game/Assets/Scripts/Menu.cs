af8210ce3151ef5ab1170379135d70fba697adeb
David Filipe <davidvfilipe@gmail.com>
2016-11-27 10:26:53 +0000

    Added COOL™ Menus.

---
+++ Vertical Scroller Killing Game/Assets/Scripts/Menu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class Menu : MonoBehaviour {
+
+
+	void Update () 
+	{
+		if ( Input.GetButtonDown ("Joystick 1 Jump") ) 
+		{
+				SceneManager.LoadScene ("Game 2");
+		}
+
+		if ( Input.GetButtonDown ("Joystick 1 Attack") ) 
+		{
+			Application.Quit();
+		}
+	}
+}
